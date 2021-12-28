a82642b7e3b0a25d81592629fb2c710d249a2824
Teo Wei Jie <wjayteo@gmail.com>
2021-12-28 11:05:00 +0800

    Update

--- prometheus/node_exporter/textfile_collectors/users.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-# /usr/share/node_exporter/users.sh
-
-USERS=$(who | wc -l)
-
-echo "# HELP node_current_users Current user count."
-echo "# TYPE node_current_users gauge"
-echo "node_current_users ${USERS}"
-
